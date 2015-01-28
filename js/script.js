284ff7529984e2c770cee1fe475731ab4ef53136
Omar Regino <omarjrz21@gmail.com>
2015-01-27 23:18:30 -0600

    Subiendo Portafolio

---
+++ js/script.js
@@ -0,0 +1,27 @@
+(function(){ 
+	var $menu 	 = $("#menu"),
+		$button  = $("#show-menu"),
+		$a 		 = $("#menu ul li a"), 
+		$chart 	 = $(".chart");
+		
+		$(".submit").attr('disabled', 'disabled');
+
+		function ShowMenu(e){
+			e.preventDefault();
+			$menu.slideToggle(); 
+		}
+
+		function ShowLink(){
+			$menu.slideToggle(); 
+		}
+
+		$button.on('click', ShowMenu);
+		$a.on('click', ShowLink);
+		$chart.easyPieChart({
+			easing: 'easeOutBounce',
+			barColor:  '#f39c12',
+			onStep: function(from, to, percent) {
+				$(this.el).find('.percent').text(Math.round(percent));
+			}
+		});
+})();
